039174da110a9698272b1fc7d8038d5c354b560f
HRHLALALA <32263355+HRHLALALA@users.noreply.github.com>
2025-03-03 05:56:44 +0000

    Deploying to gh-pages from @ HRHLALALA/hrhlalala.github.io@526849e079432bb9dcb6b061801729d24db0d27d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -368,16 +368,17 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-one-paper-accepted-in-iros",
+          title: 'One paper accepted in IROS.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-one-paper-accepted-by-ra-l",
+          title: 'One paper accepted by RA-L',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-started-postdoc-research-fellow-at-icinema-research-centre",
+          title: 'Started Postdoc Research Fellow at iCinema Research Centre.',
+          description: "",
+          section: "News",},{id: "news-one-paper-accepted-by-tnnls",
+          title: 'One paper accepted by TNNLS.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
